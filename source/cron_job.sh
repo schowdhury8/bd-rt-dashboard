8e07946266ea7a1f367bc8c4b6f9d9550e4d244f
Mahi Shafiullah <n.m.mahi@gmail.com>
2020-08-23 11:25:32 -0400

    Updated the cron job

--- source/cron_job.sh
+++ source/cron_job.sh
@@ -3,4 +3,7 @@ NOW=$(date +"%m-%d-%Y")
 export COVID_DATA_DIRECTORY="/web/nms572/covid/$NOW"
 export COVID_DEPLOY=1
 
-pipenv run python rt_computation.py
+cd "/misc/vlgscratch5/FergusGroup/nms572/software/bd-rt-dashboard/source"
+/home/nms572/.local/bin/pipenv run python rt_computation.py
+
+cp "/web/nms572/covid/$NOW" "/web/nms572/covid/latest"
